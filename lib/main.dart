764663325b1d9746f459ff3e256d2c85e592f1fa
neo24061998 <shivamsingh24680@gmail.com>
2021-03-20 11:03:56 +0530

    Day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:testing/pages/home_page.dart';
+import 'package:testing/pages/login.dart';
 
 main(List<String> args) {
   runApp(MyApp());
@@ -8,27 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Text("data"),
-        ),
-        drawer: Drawer(
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border(),
-                ),
-                child: DrawerHeader(
-                  child: Text("data"),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      theme: ThemeData.dark(),
+      //home: HomePage(),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+      initialRoute: "/home",
     );
   }
 }
